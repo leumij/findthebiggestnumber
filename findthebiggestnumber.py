3cba3252a4b33b3497f8c51620014213869e2045
leumij <cjimuel74@gmail.com>
2024-01-11 20:01:28 +0800

    main window

--- findthebiggestnumber.py
+++ findthebiggestnumber.py
@@ -7,23 +7,28 @@ from ttkthemes import ThemedTk
 # pseudocode
 
 # ask user 3 number for input
-print("Enter 3 number and let's determine the greatest among the three")
-number_one = int(input("Enter your First Number: "))
-number_two = int(input("Enter your Second Number: "))
-number_three = int(input("Enter your Third Number: "))
-    
+def find_greatest():
+    try:
+        number_one = int(entry_one.get())
+        number_two = int(entry_two.get())
+        number_three = int(entry_three.get())
 
-# find the biggest number
-if number_one > number_two and number_one > number_three:
-    greatest = number_one
-
-elif number_two > number_one and number_two > number_three:
-    greatest = number_two
-
-else:
-    greatest = number_three
+# find the greatest value
+        if number_one > number_two and number_one > number_three:
+            greatest = number_one
+        elif number_two > number_one and number_two > number_three:
+            greatest = number_two
+        else:
+            greatest = number_three
 
 # print the biggest number
-print("The greatest among the numbers you have entered {},{} and {} is number{}" .format(number_one,number_two,number_three, greatest))
+         print("The greatest among the numbers you have entered {},{} and {} is number{}" .format(number_one,number_two,number_three, greatest))
 
+    except ValueError:
+        messagebox.showerror("Error", "Please enter valid numbers.")
 
+# Create the main window
+root = tk.Tk()
+root.title("Find the Greatest Number")
+root.geometry("400x300")
+root.configure(bg="#F2F2F2")
