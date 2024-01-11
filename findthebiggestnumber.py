60c0bf1f82233dc7362a070e599c92ed7e170495
leumij <cjimuel74@gmail.com>
2024-01-11 12:05:13 +0800

    first biggest number

--- findthebiggestnumber.py
+++ findthebiggestnumber.py
@@ -7,5 +7,9 @@ print("Enter 3 number and let's determine the greatest among the three")
 number_one = int(input("Enter your First Number: "))
 number_two = int(input("Enter your Second Number: "))
 number_three = int(input("Enter your Third Number: "))
+
 # find the biggest number
+if number_one > number_two and number_one > number_three:
+    print("The largest number" , number_one)
+
 # print the biggest number
