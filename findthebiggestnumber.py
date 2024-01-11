0a288c0ffba9f86165160d93adb25633ab471e1f
leumij <cjimuel74@gmail.com>
2024-01-11 11:45:55 +0800

    pseudocode

---
+++ findthebiggestnumber.py
@@ -0,0 +1,6 @@
+# Ask user to input 3 numbers. Find and print the biggest number using only if-else statement.
+
+# pseudocode
+# ask user 3 number for input
+# find the biggest number
+# print the biggest number
