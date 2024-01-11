34007e9448a65dfb32eb4822514330a620b8f1bc
leumij <cjimuel74@gmail.com>
2024-01-11 11:59:55 +0800

    ask user to input

--- findthebiggestnumber.py
+++ findthebiggestnumber.py
@@ -1,6 +1,11 @@
 # Ask user to input 3 numbers. Find and print the biggest number using only if-else statement.
 
 # pseudocode
+
 # ask user 3 number for input
+print("Enter 3 number and let's determine the greatest among the three")
+number_one = int(input("Enter your First Number: "))
+number_two = int(input("Enter your Second Number: "))
+number_three = int(input("Enter your Third Number: "))
 # find the biggest number
 # print the biggest number
