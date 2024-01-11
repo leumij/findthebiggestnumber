196527d36dd5a9925a899c8102c9157d3ffcce81
leumij <cjimuel74@gmail.com>
2024-01-11 19:54:27 +0800

    fixing design

--- findthebiggestnumber.py
+++ findthebiggestnumber.py
@@ -1,41 +1,29 @@
 # Ask user to input 3 numbers. Find and print the biggest number using only if-else statement.
 
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
+import tkinter as tk
+from tkinter import ttk, messagebox
+from ttkthemes import ThemedTk
 
 # pseudocode
 
 # ask user 3 number for input
+print("Enter 3 number and let's determine the greatest among the three")
+number_one = int(input("Enter your First Number: "))
+number_two = int(input("Enter your Second Number: "))
+number_three = int(input("Enter your Third Number: "))
+    
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/find_greatest', methods=['POST'])
-def find_greatest():
-        number_one = int(request.form[''])
-        number_two = int(request.form[''])
-        number_three = int(request.form[''])
-                
 # find the biggest number
-        if number_one > number_two and number_one > number_three:
-            greatest = number_one
+if number_one > number_two and number_one > number_three:
+    greatest = number_one
 
-        elif number_two > number_one and number_two > number_three:
-            greatest = number_two
+elif number_two > number_one and number_two > number_three:
+    greatest = number_two
 
-        else:
-            greatest = number_three
+else:
+    greatest = number_three
 
-        return render_template('result.html', greatest=greatest)   
-
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
+# print the biggest number
+print("The greatest among the numbers you have entered {},{} and {} is number{}" .format(number_one,number_two,number_three, greatest))
 
 
